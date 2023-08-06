f44d1f2f1bf08fff6fb53d3b25dc8976f17f82f7
nyvnge <sandersonnyange@gmail.com>
2023-08-06 15:05:38 +0300

    A script that plays songs in your music directory and some changes in the previous commits

--- 0x05-Email_sending_automation_using_python/main.py
+++ 0x05-Email_sending_automation_using_python/main.py
@@ -1,52 +1,27 @@
-import tweepy
 import smtplib
 import ssl
 from email.mime.text import MIMEText
 
-#Twitter API Credentials
-consumer_key = 'ani8GL6cn90Rl5uM3emxr1F4C'
-consumer_secret = '8nJr3ItjY06bbVJCUTEVBuu8k69jE8VOM8ih3rB7kVWWBEFU4H'
-access_token = '1589515639164641280-JZoILPq4TqO41EYJ2n8GYI9dEeYw9w'
-access_token_secret = 'rGgkZDNDc1IU5S8XWHt6PogEcPWCspbkaY23P1JCxbLEJ'
-
-#Email Sender and Email receiver info
+# Email Sender and Email receiver info
 emailSender = 'sandersonnyangejr@gmail.com'
 emailPassword = 'sgllvyykiknfrcsl'
 emailReceiver = 'sanywalkerke@gmail.com'
 
-#Function to fetch latest tweets
-def getLatestTweets():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-
-    tweets = api.user_timeline(screen_name = 'MenMoneyMindset', count=1)
-    if tweets:
-        return tweets[0].text
-    else:
-        return None
-    
-#Subject and body of the email
+# Subject and body of the email
 subject = 'Daily Motivational Tweets'
+body = """Continue growing champ!
+<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Be fun and confident. you will be ahead of 90% of men.</p>&mdash; Men Money Mindset (@MenMoneyMindset) <a href="https://twitter.com/MenMoneyMindset/status/1687984584817016832?ref_src=twsrc%5Etfw">August 6, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>"""
 
-#Get latest tweet content body
-latest_tweet = getLatestTweets()
-
-if latest_tweet:
-    body = f"""Continue growing champ!\n\n{latest_tweet}"""
-else:
-    body = "No recent tweets found. "
-
-#MIMEText object
+# Create a MIMEText object with HTML content
 msg = MIMEText(body, 'html')
 msg['From'] = emailSender
 msg['To'] = emailReceiver
 msg['Subject'] = subject
 
-#Adding SSL security
+# Adding SSL security
 context = ssl.create_default_context()
 
-#Sending email
+# Sending email
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(emailSender, emailPassword)
     smtp.sendmail(emailSender, emailReceiver, msg.as_string())
