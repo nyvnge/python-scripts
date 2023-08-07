d79598b6c01e7f99a3370ec834f4f49cad43a588
nyvnge <sandersonnyange@gmail.com>
2023-08-07 23:53:03 +0300

    Few changes

--- 0x07 - Encryption and Decryption/encryption_decryption.py
+++ 0x07 - Encryption and Decryption/encryption_decryption.py
@@ -1,20 +1,37 @@
 from cryptography.fernet import Fernet
 
+#Function to encrypt plain text using Fernet encryption
 def encrypt(text, key):
     fnt = Fernet(key)
     encrypted_text = fnt.encrypt(text.encode())
     return encrypted_text
 
+#Function to decrypt encrypted text using Fernet decryption
 def decrypt(encrypted_text, key):
     fnt = Fernet(key)
     decrypted_text = fnt.decrypt(encrypted_text).decode()
     return decrypted_text
 
+#User Input
 text = input("Enter Text to process: ")
+
+#Generates random secret Key
 key = Fernet.generate_key()
+
+#Outputs the secret key
+print("Secret Key:", key.decode())
+
+#Encrypt user input using the encypt function
 encrypted_text = encrypt(text, key)
+
+#Outputs encrypted text
 print("Encrypted Text: ", encrypted_text)
 
+#Decrypt encrypted text using decrypt function
 decrypted_text = decrypt(encrypted_text, key)
+
+#Outputs decrypted text
 print("Decrypted Text: ", decrypted_text)
+
+
 
